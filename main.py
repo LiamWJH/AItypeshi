c52a0c4434e2447d7118df4ebe04f92fde37a239
Liam <liamgoesbrrr@gmail.com>
2026-09-07 22:12:30 +0900

    ended up doing nothing but a restucture for a better future

--- main.py
+++ main.py
@@ -15,127 +15,21 @@ def run_sight():
 
 threading.Thread(target=run_sight, daemon=True).start()
 
-
+class reponse:
+  def __init__(self, call_tool, response):
+    self.call_tool = call_tool
+    self.response = response
+  def say(self, ):
+    print("<AI> ", end='')
+    for chunk in response:
+      print(chunk.message.content, end='', flush=True)
+    print("")
 
 running = True
 while running:
   userinput = input("> ")
 
-  #we have to write function that does it without AI later so it gets faster
-  '''messages = [{"role": "user", "content":
-               f"""IDENTIFY THE TYPE OF CHAT THE USER WANTS FROM THE BELOW WITH THE CONDITION I GIVE:
-                  0: Normal chat/
-                  1: Sight/
-
-                  CONDITIONS FOR EACH TYPE
-                  0: Normal chat
-                  - Any single words. example: "hi", "yo", "hey", "yes", "no"
-                  - Any question that only requires elementary/middle/high school concept. examaple: "explain me entropy?", "what's a function?", "explain me fundamental physics"
-                  1: Sight
-                  - Any question or statement that directly or indirectly references things not included in question itself. example: "Whats on my screen?", "What's that round button gonna do?", "what is that image?", "look at that"
-                  - Any question/statement that requires you to see what the objective is.
-                  - Any question/statement that requires a screenshot of the screen.
-                  - Any question/statement that you don't have the real time access to.
-                  - Any question/statement that you can't answer without more user specification of the question.
-                  IF NO CONDITION IS MET
-                  1. Try finding the most fit amongst the given
-                  2. If still none is found return Normal chat.
-
-                  and return ONLY the CORRESPONDING NUMBER.
-
-                  '{userinput}'
-                  """}]
-  response = chat(
-    model='qwen3.5:9b',
-    messages=messages,
-    think=False,
-    stream=False,
-  )
-
-  print(response.message.content)
-
-  TEMPORARILY REMOVED DEVISION OF TASK
-  '''
-  '''
-    match int(response.message.content):
-      case 0:
-        messages = [{"role": "user", "content": f"set the amount of screenshots needed for this task and return the path to them: '{userinput}'"}]
-        response = chat(
-          model='qwen3.5:9b',
-          messages=messages,
-          think=False,
-          stream=False,
-          tools=[get_sight_batch]
-        )
-
-        messages.append(response.message)
-
-        if not response.message.tool_calls:
-          print(f"<AI> {response.message.content}")
-          continue
-
-        call = response.message.tool_calls[0]
-        result = get_sight_batch(**call.function.arguments)
-        messages.append({"role": "tool", "tool_name": call.function.name, "content": str(result)})
-        print(result)
-        final_messages = [{
-          "role": "user",
-          "content": f"Answer the user's question directly and concisely, using the screenshost only as supporting content if relevant. Don't describe the screenshots unless asked. Whenever the user refers to a screenshot or a picture they are referring to one in the screenshot provided,  Question: {userinput}",
-          "images": result
-          }]
-        final_response = chat(
-          model='qwen3.5:9b',
-          messages=final_messages,
-          think=False,
-          stream=True,
-          options={"num_ctx": 16000}
-        )
-
-        print("<AI> ", end='')
-        for chunk in final_response:
-          print(chunk.message.content, end='', flush=True)
-        print("")
-      case 1:
-        messages = [{"role": "user", "content": f"set the amount of screenshots needed for this task and return the path to them: '{userinput}'"}]
-        response = chat(
-          model='qwen3.5:9b',
-          messages=messages,
-          think=False,
-          stream=False,
-          tools=[get_sight_batch]
-        )
-
-        messages.append(response.message)
-
-        if not response.message.tool_calls:
-          print(f"<AI> {response.message.content}")
-          continue
-
-        call = response.message.tool_calls[0]
-        result = get_sight_batch(**call.function.arguments)
-        messages.append({"role": "tool", "tool_name": call.function.name, "content": str(result)})
-        print(result)
-        final_messages = [{
-          "role": "user",
-          "content": f"Answer the user's question directly and concisely, using the screenshost only as supporting content if relevant. Don't describe the screenshots unless asked. Question: {userinput}",
-          "images": result
-          }]
-        final_response = chat(
-          model='qwen3.5:9b',
-          messages=final_messages,
-          think=False,
-          stream=True,
-          options={"num_ctx": 16000}
-        )
-
-        print("<AI> ", end='')
-        for chunk in final_response:
-          print(chunk.message.content, end='', flush=True)
-        print("")
-
-  '''
-
-  messages = [{"role": "user", "content": f"Determine what action is needed for this task, for mouse/keyboard related actions, assume the cursor is already set to the right position. : '{userinput}'"}]
+  messages = [{"role": "user", "content": f"Determine if an action is needed for this task, and if so which task. for mouse/keyboard related actions, assume the cursor is already set to the right position. : '{userinput}'"}]
   response = chat(
     model='qwen3.5:9b',
     messages=messages,
@@ -143,44 +37,68 @@ while running:
     stream=False,
     tools=[get_sight_batch, click_mouse, pause_for]
   )
-  messages.append(response.message)
 
+
+  messages.append(response.message.content)
 
   if not response.message.tool_calls:
-    print(f"<AI> {response.message.content}")
-    continue
-
-  call = response.message.tool_calls[0]
-
-  if call.function.name == "pause_for":
-      pause_for(**call.function.arguments)
-      print(f"<AI> waited for {call.function.arguments["t"]} seconds")
-      continue
-
-  if call.function.name == "click_mouse":
-    threading.Thread(target=click_mouse, kwargs=call.function.arguments, daemon=True).start()
-    #result = click_mouse()
-    print(f"<AI> clicked mouse")
-    continue
-
-  if call.function.name == "get_sight_batch":
-    result = get_sight_batch(**call.function.arguments)
-    messages.append({"role": "tool", "tool_name": call.function.name, "content": str(result)})
-    print(result)
-    final_messages = [{
-      "role": "user",
-      "content": f"Answer the user's question directly and concisely, using the screenshost only as supporting content if relevant. Don't describe the screenshots unless asked. Whenever the user refers to a screenshot or a picture they are referring to one in the screenshot provided, Question: {userinput}",
-      "images": result
-    }]
-    final_response = chat(
+    messages = [{"role": "user", "content": f"Answer the question/statement precisley and in a clean summarized way. : '{userinput}'"}]
+    response = chat(
       model='qwen3.5:9b',
-      messages=final_messages,
+      messages=messages,
       think=False,
       stream=True,
-      options={"num_ctx": 16000}
     )
+
     print("<AI> ", end='')
-    for chunk in final_response:
+    for chunk in response:
       print(chunk.message.content, end='', flush=True)
     print("")
     continue
+
+  call = response.message.tool_calls[0]
+  call_result = None
+
+  # match tool -> use tool -> notify tool was used -> break
+  # what we want to add: know user input + AI response -> after each response store to DB or json file or smth for tool to pull
+  # if we end up using json we use classes bc they look cool and nice
+  # i should probably do OOP so it is cleaner.
+  # userinput variable + the print that was used = one chat
+  # fuck DB i dont want to learn sql im going for json.
+  # so i guess store the userinput and response as a class? or maybe just add to the json file after each response?
+  # but then i have to print the result too so it becomes repetitve and dirty to see
+  # maybe i should make a response class and make a inherited class of tool_response
+  # lets try and roll back if it's shi
+  match call.function.name:
+    case "pause_for":
+      pause_for(**call.function.arguments)
+      print(f"<AI> waited for {call.function.arguments["t"]} seconds")
+      break
+    case "click_mouse":
+      threading.Thread(target=click_mouse, kwargs=call.function.arguments, daemon=True).start()
+      print(f"<AI> clicked mouse")
+      break
+    case "get_sight_batch":
+      call_result = get_sight_batch(**call.function.arguments)
+      messages.append({"role": "tool", "tool_name": call.function.name, "content": str(call_result)})
+      print(call_result)
+
+      final_messages = [{
+        "role": "user",
+        "content": f"Answer the user's question directly and concisely, using the screenshost only as supporting content if relevant. Don't describe the screenshots unless asked. Whenever the user refers to a screenshot or a picture they are referring to one in the screenshot provided, Question: {userinput}",
+        "images": call_result
+      }]
+      final_response = chat(
+        model='qwen3.5:9b',
+        messages=final_messages,
+        think=False,
+        stream=True,
+        options={"num_ctx": 16000}
+      )
+
+      print("<AI> ", end='')
+      for chunk in final_response:
+        print(chunk.message.content, end='', flush=True)
+      print("")
+      break
+  continue
