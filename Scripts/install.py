f87b863ca25b057c4dd11b7cc932f40d186a66f1
Liam <liamgoesbrrr@gmail.com>
2026-09-12 21:24:35 +0900

    sucessfully pip, install ollama, qwen3.5:9b. Doesnt check if those already exists tho

--- Scripts/install.py
+++ Scripts/install.py
@@ -1,29 +1,21 @@
-'''pip install "pyautogui"
-pip install "opencv-python"
-pip install "sentence_transformers"
-pip install "mss"
-pip install "scikit-image"
-'''
-
 import subprocess
 import platform
 
-def run_command(command: list[str], shell=False):
-    proccess = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+def run_command(command: list[str]):
+    proccess = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, encoding="utf-8")
 
     for output in proccess.stdout:
         print(output, end='')
 
-'''run_command(["pip", "install", "pyautogui"])
+run_command(["pip", "install", "pyautogui"])
 run_command(["pip", "install", "opencv-python"])
 run_command(["pip", "install", "sentence_transformers"])
 run_command(["pip", "install", "mss"])
 run_command(["pip", "install", "scikit-image"])
-run_command(["pip", "install", ])
-'''
+
 match platform.system():
     case "Windows":
-        run_command(["irm" "https://ollama.com/install.ps1", "|", "iex"], shell=True)
+        run_command(["powershell", "-Command", "irm", "https://ollama.com/install.ps1 | iex"])
     case "Linux":
         run_command(["curl", "-fsSL", "https://ollama.com/install.sh", "|", "sh"])
     case "Darwin":
