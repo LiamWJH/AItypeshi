9f49739b66135bb803247dcd56307b199211b708
Liam <liamgoesbrrr@gmail.com>
2026-09-12 14:14:24 +0900

    adding new ways for the AI to know if past response was correct

--- main.py
+++ main.py
@@ -9,12 +9,6 @@ from sight import sight
 from tooling import *
 from tooling import _get_memory_batch
 
-class conversation_summary:
-  def __init__(self, summary, time, which_tool_used):
-    self.summary = summary
-    self.time = time
-    self.which_tool_used = which_tool_used
-
 result_queue = queue.Queue()
 def run_sight():
   asyncio.run(sight())
@@ -39,7 +33,10 @@ def say(message, stream=False, isPlain=False):
     print(f"<AI> {message.message.content}")
     return message.message.content
 
-def save_conversation_summary_for_session(user, response, whichtool):
+def save_conversation_summary_for_session(user, response, wasCorrectResp, whichtool):
+  global first_question
+  if first_question:
+    return 0;
   messages = [{"role": "user", "content":  f"Write a factual one-sentence summary of this exchange, third person, no commentary, no questions, no meta-text. Try to make it as short as possible. User said: \"{user}\" | AI replied: \"{response}\""}]
   response=chat(
     model='qwen3.5:9b',
@@ -55,6 +52,7 @@ def save_conversation_summary_for_session(user, response, whichtool):
   with open("MEMORY.json", "r") as f:
     data = json.load(f)
   data.append({
+    "correctanswer": wasCorrectResp,
     "summary": response.message.content,
     "timestamp": formatted_time,
     "usedtool": whichtool,
@@ -62,9 +60,32 @@ def save_conversation_summary_for_session(user, response, whichtool):
   with open("MEMORY.json", "w") as f:
     json.dump(data, f, indent=2, ensure_ascii=False)
 
+def was_response_correct(resp, reaction):
+  messages = [{"role": "assistant", "content": resp},
+              {"role": "user", "content": f"Answer in either 'YES' or 'NO' ONLY. Did the user like the response you gave (if these is no specific reaction to the response say 'YES')? USER: {reaction}"
+            }]
+  response = chat(
+    model='qwen3.5:9b',
+    messages=messages,
+    think=False,
+    stream=False,
+  )
+
+  if response.message.content.capitalize() == 'YES':
+    return True
+  else:
+    return False
+
+
 running = True
+
+prev_userinput = None
+prev_resp = None
+first_question = True
+
 while running:
   userinput = input("> ")
+  resp = None
 
   messages = [{"role": "user", "content": f"Determine if an action is needed for this task, and if so which task. for mouse/keyboard related actions, assume the cursor is already set to the right position. USERINPUT: '{userinput}'"}]
   response = chat(
@@ -88,7 +109,7 @@ while running:
 
     resp = say(response, stream=True)
 
-    save_conversation_summary_for_session(userinput, resp, None)
+    save_conversation_summary_for_session(prev_userinput, prev_resp, was_response_correct(prev_resp, userinput), None)
     continue
 
   call = response.message.tool_calls[0]
@@ -99,12 +120,12 @@ while running:
     case "pause_for":
       pause_for(**call_args)
       say(f"waited for {call.function.arguments["t"]} seconds", isPlain=True)
-      save_conversation_summary_for_session(userinput, f"waited for {call.function.arguments["t"]} seconds", "pause_for")
+      save_conversation_summary_for_session(prev_userinput, f"waited for {call.function.arguments["t"]} seconds", was_response_correct(prev_resp, userinput), "pause_for")
       continue
     case "click_mouse":
       threading.Thread(target=click_mouse, kwargs=call_args, daemon=True).start()
       say(f"clicked mouse", isPlain=True)
-      save_conversation_summary_for_session(userinput, f"clicked mouse", "click_mouse")
+      save_conversation_summary_for_session(userinput, f"clicked mouse", was_response_correct(prev_resp, userinput), "click_mouse")
       continue
     case "get_sight_batch":
       call_result = get_sight_batch(**call_args)
@@ -125,28 +146,29 @@ while running:
       )
 
       resp = say(final_response, stream=True)
-      save_conversation_summary_for_session(userinput, resp, "get_sight_batch")
+      save_conversation_summary_for_session(userinput, resp, was_response_correct(prev_resp, userinput), "get_sight_batch")
       continue
     case "get_memory_batch":
       print(call_args, "hi nerd")
       call_result = _get_memory_batch(**call_args, userinput=userinput)
+      messages.append({"role": "assistant", "content": response.message.content})
       messages.append({"role": "tool", "tool_name": call.function.name, "content": str(call_result)})
+      messages.append({"role": "user", "content": f"Answer the user's question directly and concisely by using the given context if relevant. Don't describe the context unless asked. Question: {userinput}"})
 
-      final_messages = [{
-        "role": "user",
-        "content": f"Answer the user's question directly and concisely by using the given context if relevant. Don't describe the context unless asked. Context: {call_result} Question: {userinput}",
-      }]
       final_response = chat(
         model='qwen3.5:9b',
-        messages=final_messages,
+        messages=messages,
         think=False,
         stream=True,
         options={"num_ctx": 16000}
       )
 
       resp = say(final_response, stream=True)
-      save_conversation_summary_for_session(userinput, resp, "get_memory_batch")
+      save_conversation_summary_for_session(userinput, resp, was_response_correct(prev_resp, userinput), "get_memory_batch")
       continue
     case _:
       raise codefuckedup
+
+  prev_userinput = userinput
+  prev_resp = resp
   continue
